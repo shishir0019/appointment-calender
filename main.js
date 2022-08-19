8a5c6d041563e360097fe049e14f4ef2a615b070
blackCat <shishir0019@gmail.com>
2022-08-19 18:24:29 +0600

    'update'

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 class DateInstence {
     #date
-    #monthName
-    constructor() {
-        this.#date = new Date()
-        this.#monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    #monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    constructor(date = new Date) {
+        this.#date = date
     }
 
     getDate() {
@@ -22,6 +21,13 @@ class DateInstence {
         return this.#monthName[this.#date.getMonth()];
     }
 
+    getNextMonth() {
+        return this.#getDaysInMonth(this.#date.getMonth() < 11 ? this.#date.getMonth() + 1 : 0, this.#date.getMonth() < 11 ? this.#date.getFullYear() : this.#date.getFullYear() + 1)
+    }
+    getPreviousMonth() {
+        return this.#getDaysInMonth(this.#date.getMonth() > 0 ? this.#date.getMonth() - 1 : 11, this.#date.getMonth() > 0 ? this.#date.getFullYear() : this.#date.getFullYear() - 1)
+    }
+
     nextMonth() {
         if (this.#date.getMonth() < 11) this.#date.setMonth(this.#date.getMonth() + 1)
         else {
@@ -38,8 +44,17 @@ class DateInstence {
         }
     }
 
-    getCurrentMonth() {
-        return this.#getDaysInMonth(this.getMonth(), this.getFullYear())
+    getCurrentMonth(fullCalender = false) {
+        let preMonthDate = []
+        let nextMonthDate = []
+        for (let i = 0; i < this.#date.getDay(); i++) {
+            preMonthDate.push(this.getPreviousMonth()[(this.getPreviousMonth().length - 1) - i])
+        }
+        for (let i = 0; i < 42 - (this.#getDaysInMonth(this.#date.getMonth(), this.#date.getFullYear()).length + preMonthDate.length); i++) {
+            nextMonthDate.push(this.getNextMonth()[i])
+        }
+        if (fullCalender) return [...preMonthDate.reverse(), ...this.#getDaysInMonth(this.getMonth(), this.getFullYear()), ...nextMonthDate];
+        else return this.#getDaysInMonth(this.getMonth(), this.getFullYear())
     }
 
     #getDaysInMonth(month, year) {
@@ -53,33 +68,106 @@ class DateInstence {
     }
 }
 
-const date = new DateInstence();
+const dateInstence = new DateInstence();
+
+let state = localStorage.state ? JSON.parse(localStorage.state) : {
+    appointmentList: []
+}
+
+const save = () => {
+    localStorage.state = JSON.stringify(state)
+    state = JSON.parse(localStorage.state) || {}
+}
 
 const nextMonth = () => {
-    date.nextMonth();
-    printMonth(date.getMonth(), date.getFullYear())
+    dateInstence.nextMonth();
+    printMonth(dateInstence.getMonth(), dateInstence.getFullYear())
 }
 
 const previousMonth = () => {
-    date.previousMonth();
-    printMonth(date.getMonth(), date.getFullYear())
+    dateInstence.previousMonth();
+    printMonth(dateInstence.getMonth(), dateInstence.getFullYear())
 }
 
 const printMonth = (month, year) => {
-    console.log(date.monthName());
+    let today = new Date()
+    today.setUTCHours(0, 0, 0, 0)
     let days = document.querySelector('.days');
     let monthDiv = document.querySelector('.month');
 
     days.innerHTML = ''
     monthDiv.innerHTML = ''
 
-    monthDiv.innerHTML = date.monthName() + ', ' + date.getFullYear()
+    monthDiv.innerHTML = dateInstence.monthName() + ', ' + dateInstence.getFullYear()
 
-    for (const day of date.getCurrentMonth().map(day => day.getDate())) {
-        let dayElement = document.createElement('div');
-        dayElement.innerHTML = day
-        days.appendChild(dayElement);
+    for (const day of dateInstence.getCurrentMonth(true)) {
+        day.setUTCHours(0, 0, 0, 0)
+        let isToday = day.getTime() == today.getTime()
+        let dayElement = document.createElement('button');
+
+        dayElement.classList.add('days__item')
+        if (day.getMonth() !== dateInstence.getMonth()) dayElement.setAttribute('disabled', '');
+        if (isToday) dayElement.classList.add('days__item--today')
+        dayElement.innerHTML = day.getDate()
+        days.append(dayElement);
+
+        let appointments = state.appointmentList.filter(appointment => new Date(appointment.date).getTime() == day.getTime())
+        let listElement = document.createElement('div');
+        listElement.classList.add('appointments')
+        for (const appointment of appointments) {
+            let appointmentItem = document.createElement('div')
+            appointmentItem.innerHTML = appointment.first_name
+            appointmentItem.classList.add('appointments__item')
+            listElement.appendChild(appointmentItem)
+        }
+        dayElement.appendChild(listElement)
     }
 }
 
-printMonth(date.getMonth(), date.getFullYear())
+printMonth(dateInstence.getMonth(), dateInstence.getFullYear())
+
+const createModal = document.querySelector('#createAppointmentDialog');
+
+if (typeof createModal.showModal !== 'function') {
+    createModal.hidden = true;
+}
+
+const openCreateDialog = () => {
+    if (typeof createModal.showModal === "function") {
+        createModal.showModal();
+    } else {
+        outputBox.value = "Sorry, the <dialog> API is not supported by this browser.";
+    }
+}
+
+const closeCreateDialog = () => {
+    if (typeof createModal.showModal === "function") {
+        createModal.close();
+    } else {
+        outputBox.value = "Sorry, the <dialog> API is not supported by this browser.";
+    }
+}
+
+// createModal.addEventListener('close', (e) => {
+//     console.log('close', e);
+// });
+
+createModal.addEventListener('submit', (formDataEvent) => {
+    let formData = new FormData(formDataEvent.target);
+
+    let first_name = formData.get('first_name');
+    let last_name = formData.get('last_name');
+    let email = formData.get('email');
+    let age = formData.get('age');
+    let gender = formData.get('gender');
+    let date = formData.get('date');
+    let time = formData.get('time');
+
+    let appointment = { first_name, last_name, email, age, gender, date, time }
+
+    state.appointmentList.push(appointment);
+
+    save()
+    printMonth(dateInstence.getMonth(), dateInstence.getFullYear())
+});
+
